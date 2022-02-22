07e72c67feed598c469c95fd5940ec4b125869c4
msprks <msparks@MEs-MacBook-Air.local>
2022-02-22 13:46:19 -0600

    change

--- in_your_painting/script.js
+++ in_your_painting/script.js
@@ -19,39 +19,41 @@ poemList=[
   // poem9a,
   poem10,
   // poem10a,
-  poem11
+  poem11,
+  poem11a
 ];
 let p = 0;
 
 window.onload = ()=>{
 
   let wrapper = document.getElementById("body");
+  makeNewPoem();
 
   wrapper.addEventListener("click",()=>{
-
-//  let image = document.getElementById("backgroundimage");
-  let wrapper = document.getElementById("outputwrapper");
-  let poem  = document.createElement("p");
-
-  // image.setAttribute("src","images/2.jpg");
-  let num = Math.floor(1+(Math.random()*50));
-//  console.log(num);
-//  image.setAttribute("src","images/"+num+".jpg");
-  let newpoem = poemList[p]()+".";
-  poem.innerText = newpoem;
-  console.log(newpoem);
-
-  wrapper.appendChild(poem);
-
-    p++;
-    if(p>=poemList.length){
-      p=0;
-    }
-
+    makeNewPoem();
   });
-
 }
 
+function makeNewPoem(){
+  //  let image = document.getElementById("backgroundimage");
+    let outputwrapper = document.getElementById("outputwrapper");
+    let poem  = document.createElement("p");
+
+    // image.setAttribute("src","images/2.jpg");
+    let num = Math.floor(1+(Math.random()*50));
+  //  console.log(num);
+  //  image.setAttribute("src","images/"+num+".jpg");
+    let newpoem = poemList[p]();
+    poem.innerHTML = newpoem;
+    console.log(newpoem);
+
+    outputwrapper.appendChild(poem);
+
+      p++;
+      if(p>=poemList.length){
+        p=0;
+      }
+}
 // function fadeout(element){
 //   if(element.style.opacity>0){
 //     element.style.opacity+= -1;
@@ -69,7 +71,7 @@ function random(wordArr){
 //poemsbelow
 
 function poem1(){
-	let poem = "In your house I was "+random(VERBSVBG)+" and "+random(VERBSVBG);
+	let poem = "In your house I was "+random(VERBSVBG)+" and "+random(VERBSVBG)+".";
 	return poem;
 }
 
@@ -80,32 +82,32 @@ function poem1(){
 
 
 function poem2(){
-	let poem = "In your chair I thought about my "+random(ADJECTIVES)+" "+random(NOUNS);
+	let poem = "In your chair I thought about my "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
 function poem3(){
-	let poem = "In your book I read about the "+random(ADJECTIVES)+" "+random(NOUNS);
+	let poem = "In your book I read about the "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
 function poem3a(){
-  let poem = "In your "+random(NOUNS)+" I "+random(VERBSPAST);
+  let poem = "In your "+random(NOUNS)+" I "+random(VERBSPAST)+".";
 	return poem;
 }
 
 function poem4(){
-  let poem = "In your drawer I touched your "+random(ADJECTIVES)+" "+random(NOUNS);
+  let poem = "In your drawer I touched your "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
 function poem4a(){
-  let poem = "In your photo I saw "+random(NAMES);
+  let poem = "In your photo I saw "+random(NAMES)+".";
   return poem;
 }
 
 function poem5(){
-  let poem = "In your closet I searched for the "+random(ADJECTIVES)+" "+random(NOUNS);
+  let poem = "In your closet I searched for the "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
@@ -115,36 +117,41 @@ function poem5(){
 // }
 
 function poem6(){
-  let poem = "In your bed I uncovered my "+random(ADJECTIVES)+" "+random(NOUNS);
+  let poem = "In your bed I uncovered my "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
 function poem7(){
-  let poem = "In your dream I watched my "+random(NOUNS)+" "+random(VERBSVBG);
+  let poem = "In your dream I watched my "+random(NOUNS)+" "+random(VERBSVBG)+".";
 	return poem;
 }
 
 function poem7a(){
-  let poem = "In your "+random(NOUNS)+" I "+random(VERBSPAST);
+  let poem = "In your "+random(NOUNS)+" I "+random(VERBSPAST)+".";
 	return poem;
 }
 
 function poem8(){
-  let poem = "In your kitchen I tasted "+random(ADJECTIVES)+" "+random(NOUNS);
+  let poem = "In your kitchen I tasted "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
 function poem9(){
-  let poem = "In your yard I stepped on the "+random(ADJECTIVES)+" "+random(NOUNS);
+  let poem = "In your yard I stepped on the "+random(ADJECTIVES)+" "+random(NOUNS)+".";
 	return poem;
 }
 
 function poem10(){
-  let poem = "In your studio I found the "+random(NOUNS)+" "+random(VERBSVBG);
+  let poem = "In your studio I found the "+random(NOUNS)+" "+random(VERBSVBG)+".";
 	return poem;
 }
 
 function poem11(){
-  let poem = "In your painting I saw the "+random(NOUNS)+"<br><br>";
+  let poem = "In your painting I saw the "+random(NOUNS)+".";
+	return poem;
+}
+
+function poem11a(){
+  let poem = "&#8205;";
 	return poem;
 }
